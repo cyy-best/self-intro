783d52d8976b9280bb177305a70b8b1ad7b99a3e
yucao <Cy871209@163.com>
2023-07-05 15:32:55 -0230

    UI

--- accounts/public/javascripts/main.js
+++ accounts/public/javascripts/main.js
@@ -5,3 +5,18 @@ $('#time').datepicker({
     todayBtn: true,
     todayHighlight: true,
 })
+
+function test() {
+    let pwd, pwdAgain;
+    do {
+        pwd = prompt('Enter password');
+    }
+    while( parseInt(pwd) !== 123456)
+    // do {
+    //     pwdAgain = prompt('Please enter passcode again');
+        
+    // }
+    // while (pwd != pwdAgain)
+        alert('密码正确');
+    
+}
